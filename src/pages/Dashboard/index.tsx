b376a90b4a2733a634cf90c6008ae9caee477448
Thiago Mendes <thiagoromendes@gmail.com>
2020-10-09 22:06:41 -0300

    Implementado consulta a API para retorno de alimentos da tela de Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -59,7 +59,10 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      await api.get('/foods').then(response => {
+        setFoods(response.data)
+      });
+
     }
 
     loadFoods();
@@ -67,7 +70,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      api.get('/categories').then(response => {
+        setCategories(response.data)
+      })
     }
 
     loadCategories();
